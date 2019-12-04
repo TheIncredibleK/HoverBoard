b2382ac4570418caaca40fb6bb9b48897443d51b
Keith Kenny <keith.kny@gmail.com>
2019-12-04 20:54:22 +0000

    bla

---
+++ Assets/Generators/HoverBoard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverBoard : MonoBehaviour {
+
+
+    // Components that we can change to affect visuals
+    [SerializeField]
+    private int sections;
+    [SerializeField]
+    private float rotSpeed;
+    [SerializeField]
+    private float sizeLargest;
+    [SerializeField]
+    private float topSize;
+    [SerializeField]
+    private GameObject corePrefab;
+    [SerializeField]
+    private float distFromEachother;
+
+
+    List<GameObject> mySections;
+	// Use this for initialization
+	void Start () {
+        mySections = new List<GameObject>();
+        var sizeChange = sizeLargest / sections;
+		for(int i = 0; i <= sections; i++)
+        {
+            var currentPiece = Instantiate(corePrefab, this.transform);
+            currentPiece.transform.position = this.transform.position + Vector3.up * (distFromEachother * i + 1);
+            currentPiece.transform.localScale = new Vector3(sizeLargest - (sizeChange * i + 1), .02f, 1 - (sizeChange * i + 1));
+            mySections.Add(currentPiece);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var flipFlop = 1;
+		foreach(var piece in mySections)
+        {
+            piece.transform.Rotate(piece.transform.up * rotSpeed * flipFlop * Time.deltaTime);
+            flipFlop *= -1;
+        }
+    }
+}
---
+++ Assets/Movement/HoverboardController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverboardController : MonoBehaviour {
+
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float maxSpeed;
+
+    // Completely private variables
+    private float horizontal;
+    private float vertical;
+
+    private Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        SetDirections();
+        myRigidBody.AddForce((Vector3.right * horizontal * acceleration * Time.deltaTime) + 
+        (Vector3.forward * vertical * acceleration) * Time.fixedDeltaTime, ForceMode.Acceleration);
+        myRigidBody.velocity = Vector3.ClampMagnitude(myRigidBody.velocity, maxSpeed);
+	
+    }
+
+
+    private void SetDirections()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log("Horizontal= " + horizontal);
+        vertical = Input.GetAxis("Vertical");
+        Debug.Log("Vertical= " + vertical);
+    }
+}
